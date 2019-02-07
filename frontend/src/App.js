20618730cd3cd0a453018da4a48edd701bf6d6f7
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-02-07 16:37:01 -0200

    create basic pages and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Timeline from './pages/Timeline';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/timeline" component={Timeline} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
