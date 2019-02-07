92ff9fd918ae3b5da99b24170f5a8418a65f1c64
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-02-07 17:46:57 -0200

    implement pages Login and Timeline

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,7 +1,45 @@
 import React, { Component } from 'react';
 
+import twitterLogo from '../twitter.svg';
+import './Timeline.css';
+
 export default class Timeline extends Component {
+    
+    state = {
+        newTweet: ''
+    };
+
+    handleNewTweet = (e) => {
+        
+        if (e.keyCode !== 13) return;
+
+        const content = this.state.newTweet;
+        const author = localStorage.getItem("@GoTweeter:username");
+
+        console.log(content, author);
+    };
+
+    handleInputChange = (e) => {
+        this.setState({ newTweet: e.target.value });
+    };
+
     render() {
-        return <h1>Timeline</h1>;
-    }
+        return (
+            <div className="timeline-wrapper">
+                <img src={twitterLogo} height={24} alt="GoTwitter" />
+
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder="O que está acontencedo?"/>
+                </form>
+
+                {/* <ul>
+                    
+                </ul> */}
+            </div>
+        );
+    };
 }
