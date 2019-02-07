1ce56e110c2d692a6ecd449e9c72fd78dbd36b3f
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-02-07 00:53:28 -0200

    include moongose dependencie into app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://omni:omni123@ds125385.mlab.com:25385/omnistack-backend-db');
+
 app.get('/', (req, res) => {
-    return res.send('Hello World Node!');
+    return res.send('Hello World NodeJS!');
 });
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+    console.log(':) Server started on port 3000');
 });
