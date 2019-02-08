183cd18b3b3bec82dd92112adc3e67fb23a3ae8a
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-02-07 23:39:25 -0200

    implement socket.io on frontend

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import socket from 'socket.io-client';
+import api from '../services/api';
 
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
 
+import Tweet from '../components/Tweet';
+
 export default class Timeline extends Component {
     
     state = {
+        tweets: [],
         newTweet: ''
     };
 
-    handleNewTweet = (e) => {
+    async componentDidMount() {
+        this.subscribeToEvents();
+        const response = await api.get('tweets');
+
+        this.setState({ tweets: response.data });
+    };
+
+    subscribeToEvents = () => {
+        const io = socket('http://localhost:3000');
+        io.on('tweet', data => {
+            this.setState({ tweets: [data, ...this.state.tweets] });
+        });
+        io.on('like', data => {
+            this.setState({ tweets: this.state.tweets.map( tweet =>
+                tweet._id == data._id ? data : tweet
+                )
+            })
+        });
+    };
+
+    handleNewTweet = async (e) => {
         
         if (e.keyCode !== 13) return;
 
         const content = this.state.newTweet;
         const author = localStorage.getItem("@GoTweeter:username");
 
-        console.log(content, author);
+        await api.post('tweets', { content, author });
+
+        this.setState({ newTweet: '' });
     };
 
     handleInputChange = (e) => {
@@ -36,9 +63,7 @@ export default class Timeline extends Component {
                         placeholder="O que está acontencedo?"/>
                 </form>
 
-                {/* <ul>
-                    
-                </ul> */}
+                <ul className="tweet-list">{ this.state.tweets.map(tweet => (<Tweet key={tweet._id} tweet={tweet} />)) }</ul>
             </div>
         );
     };
