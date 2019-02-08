8938b8e8c9de83dc4cf3d8a2b394207dc01d113c
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-02-08 00:02:29 -0200

    some comments

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -15,13 +15,13 @@ export default class Login extends Component {
 
     handleSubmit = (e) => {
         
-        e.preventDefault();
+        e.preventDefault(); // break the form or button submit
         const { username } = this.state;
         
         if (!username.length) return;
 
-        localStorage.setItem('@GoTweeter:username', username);
-        this.props.history.push('/timeline');
+        localStorage.setItem('@GoTweeter:username', username); // insert data on local storage (cab be show with browser)
+        this.props.history.push('/timeline'); // redirect to timeline
     };
 
     render() {
