71fb76d0034813b65cb75b618f02ff0d816c63a0
Vinicius Serpa <vinicius.serpa@gmail.com>
2019-02-10 00:28:35 -0200

    component Tweet created to list tweets in timeline

--- mobile/src/pages/Timeline.js
+++ mobile/src/pages/Timeline.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../services/api';
+import Tweet from '../components/Tweet';
 
 export default class Timeline extends Component {
 
@@ -15,7 +16,7 @@ export default class Timeline extends Component {
         title: "Inicio",
         headerRight: (
             <TouchableOpacity onPress={() => {}}>
-                <Icon style={{ marginRight: 20 }} name="add-circle-outline" sie={24} color="#4BB0EE"></Icon>
+                <Icon style={{ marginRight: 20 }} name="add-circle-outline" size={24} color="#4BB0EE"></Icon>
             </TouchableOpacity>
         )
     };
@@ -28,7 +29,14 @@ export default class Timeline extends Component {
   render() {
         return (
           <View style={styles.container}>
-            {this.state.tweets.map(tweet => <Text>{tweet.author}</Text>)}
+            {/* {this.state.tweets.map(tweet => <Text>{tweet.author}</Text>)} */}
+
+            <FlatList 
+                data={this.state.tweets} 
+                keyExtractor={tweet => tweet._id} 
+                renderItem={({ item }) => <Tweet tweet={item}/> } 
+            />
+
           </View>
         );
     }
